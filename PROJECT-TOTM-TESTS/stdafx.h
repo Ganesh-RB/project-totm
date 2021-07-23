794a9341335ef56dd918caac0273c9947bedbef2
tyagio <80507825+tyagio@users.noreply.github.com>
2021-07-23 11:10:02 +0530

    added stdafx of tests

---
+++ PROJECT-TOTM-TESTS/stdafx.h
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+#include <stack>
+#include <memory>
+#include <map>
+#include <list>
+#include <assert.h>
+#include <cmath>
+#include <vector>
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
+
+#include "gtest/gtest.h"
