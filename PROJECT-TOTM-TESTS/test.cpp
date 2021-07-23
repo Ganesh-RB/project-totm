4f727912f47939a4c8fc51d240aaaeb7d3d16be6
tyagio <80507825+tyagio@users.noreply.github.com>
2021-07-23 13:40:15 +0530

    some more tests

--- PROJECT-TOTM-TESTS/test.cpp
+++ PROJECT-TOTM-TESTS/test.cpp
@@ -17,7 +17,7 @@ TEST(Stateman, PoppingOrReplacingWhenEmpty) {
 	test_state_stack.ProcessStatechange();
 	EXPECT_EQ(test_state_stack.no_of_states(), 1);
 }
-TEST(Stateman, InitOfState) {
+TEST(Stateman,InitOfState) {
 	stateman test_state_stack;
 	EXPECT_EQ(test_state_stack.no_of_states(), 0);
 	int test_me;
@@ -26,10 +26,10 @@ TEST(Stateman, InitOfState) {
 	test_state_stack.ProcessStatechange();
 	EXPECT_EQ(test_me, dummy_state::code_types::Initialized_or_running);
 }
-TEST(Stateman, MultipleAddCommandsIn1Cycle) {
+TEST(Stateman,MultipleAddCommandsIn1Cycle) {
 	stateman test_state_stack;
 	EXPECT_EQ(test_state_stack.no_of_states(), 0);
-	int test_me, test_me_2;
+	int test_me,test_me_2;
 	test_state_stack.Add(std::make_unique<dummy_state>(test_me));
 	test_state_stack.Add(std::make_unique<dummy_state>(test_me_2));
 	test_state_stack.ProcessStatechange();
@@ -41,7 +41,7 @@ TEST(Stateman, MultipleAddCommandsIn1Cycle) {
 TEST(Stateman, MultiplePopCommandsIn1Cycle) {
 	stateman test_state_stack;
 	EXPECT_EQ(test_state_stack.no_of_states(), 0);
-	int test_me, test_me_2;
+	int test_me,test_me_2;
 	test_state_stack.Add(std::make_unique<dummy_state>(test_me));
 	test_state_stack.ProcessStatechange();
 	test_state_stack.Add(std::make_unique<dummy_state>(test_me_2));
@@ -59,7 +59,7 @@ TEST(Stateman, MultiplePopCommandsIn1Cycle) {
 TEST(Stateman, AddAndPopCommandsIn1Cycle) {
 	stateman test_state_stack;
 	EXPECT_EQ(test_state_stack.no_of_states(), 0);
-	int test_me, test_me_2;
+	int test_me,test_me_2;
 	test_state_stack.Add(std::make_unique<dummy_state>(test_me));
 	test_state_stack.ProcessStatechange();
 	test_state_stack.Add(std::make_unique<dummy_state>(test_me_2));
@@ -70,10 +70,10 @@ TEST(Stateman, AddAndPopCommandsIn1Cycle) {
 	EXPECT_EQ(test_me_2, dummy_state::code_types::Initialized_or_running);
 }
 
-TEST(Stateman, IsLowerStatePaused) {
+TEST(Stateman,IsLowerStatePaused) {
 	stateman test_state_stack;
 	EXPECT_EQ(test_state_stack.no_of_states(), 0);
-	int test_me, test_me_2;
+	int test_me,test_me_2;
 	test_state_stack.Add(std::make_unique<dummy_state>(test_me));
 	test_state_stack.ProcessStatechange();
 	test_state_stack.Add(std::make_unique<dummy_state>(test_me_2));
@@ -90,7 +90,7 @@ TEST(Assetholder_DeathTest, invalidgetfont) {
 
 TEST(Assetholder_DeathTest, invalidgettexture) {
 	asset_holder test_assets;
-	EXPECT_DEATH(test_assets.get_texture(asset_holder::group_member_name::OJJAS, 0), "Assertion failed.*");
+	EXPECT_DEATH(test_assets.get_texture(asset_holder::group_member_name::OJJAS,0), "Assertion failed.*");
 }
 
 TEST(Assetholder_DeathTest, invalidgetsoundbuffer) {
@@ -121,8 +121,44 @@ TEST(AssetholderProperLoading, validsoundbuffer) {
 	EXPECT_NO_CRASH(test_assets.get_soundbuffer(asset_holder::group_member_name::OJJAS, asset_holder::ojjas_sounds::BUTTON_FORWARD));
 }
 
-TEST(AssetholderProperLoading, validplay) {
+TEST(AssetholderProperLoading, validplaysound) {
 	asset_holder test_assets;
 	test_assets.add_sound_buffer(asset_holder::group_member_name::OJJAS, asset_holder::ojjas_sounds::BUTTON_FORWARD, "Sounds/button_forward.flac");
 	EXPECT_NO_CRASH(test_assets.play_sound(asset_holder::group_member_name::OJJAS, asset_holder::ojjas_sounds::BUTTON_FORWARD));
+}
+
+TEST(AssetholderOverwrite, overwritefont) {
+	asset_holder test_assets;
+	test_assets.add_font( 0, "Fonts/Dosis-ExtraLight.otf" );
+	test_assets.add_font( 0, "Fonts/Dosis-Light.otf");
+	EXPECT_NO_CRASH(test_assets.get_font(0));
+}
+
+TEST(AssetholderOverwrite, overwritetexture) {
+	asset_holder test_assets;
+	test_assets.add_texture(asset_holder::group_member_name::OJJAS, asset_holder::ojjas_textures::SPRING, "Images/spring.png");
+	test_assets.add_texture(asset_holder::group_member_name::OJJAS, asset_holder::ojjas_textures::SPRING, "Images/teleporter.png");
+	EXPECT_NO_CRASH(test_assets.get_texture(asset_holder::group_member_name::OJJAS, asset_holder::ojjas_textures::SPRING));
+}
+
+TEST(AssetholderOverwrite, overwritesoundbuffer) {
+	asset_holder test_assets;
+	test_assets.add_sound_buffer(asset_holder::group_member_name::OJJAS, asset_holder::ojjas_sounds::BUTTON_FORWARD, "Sounds/button_forward.flac");
+	test_assets.add_sound_buffer(asset_holder::group_member_name::OJJAS, asset_holder::ojjas_sounds::BUTTON_FORWARD, "Sounds/button_backward.flac");
+	EXPECT_NO_CRASH(test_assets.get_soundbuffer(asset_holder::group_member_name::OJJAS, asset_holder::ojjas_sounds::BUTTON_FORWARD));
+}
+
+TEST(AssetholderInvalidAddress,InvalidFontFile) {
+	asset_holder test_assets;
+	EXPECT_NO_CRASH(test_assets.add_font(0, "Invalid_font.otf"));
+}
+
+TEST(AssetholderInvalidAddress, InvalidTextureFile) {
+	asset_holder test_assets;
+	EXPECT_NO_CRASH(test_assets.add_texture(asset_holder::group_member_name::OJJAS, 0,"Invalid_texture.png"));
+}
+
+TEST(AssetholderInvalidAddress, InvalidSoundbufferFile) {
+	asset_holder test_assets;
+	EXPECT_NO_CRASH(test_assets.add_sound_buffer(asset_holder::group_member_name::OJJAS, 0, "Invalid_soundbuffer.wav"));
 }
