ee3fbcbd204e12e255520e686c4d1eb79d3fac29
tyagio <80507825+tyagio@users.noreply.github.com>
2021-07-23 12:22:09 +0530

    added a macro,some extra tests

--- PROJECT-TOTM-TESTS/test.cpp
+++ PROJECT-TOTM-TESTS/test.cpp
@@ -2,6 +2,10 @@
 #include "dummy_state.h"
 #include "context.h"
 
+
+# define EXPECT_NO_CRASH(statement) \
+   EXPECT_EXIT((statement,exit(0)),::testing::ExitedWithCode(0),".*")
+
 TEST(Stateman, PoppingOrReplacingWhenEmpty) {
 	stateman test_state_stack;
 	EXPECT_EQ(test_state_stack.no_of_states(), 0);
@@ -13,7 +17,7 @@ TEST(Stateman, PoppingOrReplacingWhenEmpty) {
 	test_state_stack.ProcessStatechange();
 	EXPECT_EQ(test_state_stack.no_of_states(), 1);
 }
-TEST(Stateman,InitOfState) {
+TEST(Stateman, InitOfState) {
 	stateman test_state_stack;
 	EXPECT_EQ(test_state_stack.no_of_states(), 0);
 	int test_me;
@@ -22,10 +26,10 @@ TEST(Stateman,InitOfState) {
 	test_state_stack.ProcessStatechange();
 	EXPECT_EQ(test_me, dummy_state::code_types::Initialized_or_running);
 }
-TEST(Stateman,MultipleAddCommandsIn1Cycle) {
+TEST(Stateman, MultipleAddCommandsIn1Cycle) {
 	stateman test_state_stack;
 	EXPECT_EQ(test_state_stack.no_of_states(), 0);
-	int test_me,test_me_2;
+	int test_me, test_me_2;
 	test_state_stack.Add(std::make_unique<dummy_state>(test_me));
 	test_state_stack.Add(std::make_unique<dummy_state>(test_me_2));
 	test_state_stack.ProcessStatechange();
@@ -37,7 +41,7 @@ TEST(Stateman,MultipleAddCommandsIn1Cycle) {
 TEST(Stateman, MultiplePopCommandsIn1Cycle) {
 	stateman test_state_stack;
 	EXPECT_EQ(test_state_stack.no_of_states(), 0);
-	int test_me,test_me_2;
+	int test_me, test_me_2;
 	test_state_stack.Add(std::make_unique<dummy_state>(test_me));
 	test_state_stack.ProcessStatechange();
 	test_state_stack.Add(std::make_unique<dummy_state>(test_me_2));
@@ -55,7 +59,7 @@ TEST(Stateman, MultiplePopCommandsIn1Cycle) {
 TEST(Stateman, AddAndPopCommandsIn1Cycle) {
 	stateman test_state_stack;
 	EXPECT_EQ(test_state_stack.no_of_states(), 0);
-	int test_me,test_me_2;
+	int test_me, test_me_2;
 	test_state_stack.Add(std::make_unique<dummy_state>(test_me));
 	test_state_stack.ProcessStatechange();
 	test_state_stack.Add(std::make_unique<dummy_state>(test_me_2));
@@ -66,10 +70,10 @@ TEST(Stateman, AddAndPopCommandsIn1Cycle) {
 	EXPECT_EQ(test_me_2, dummy_state::code_types::Initialized_or_running);
 }
 
-TEST(Stateman,IsLowerStatePaused) {
+TEST(Stateman, IsLowerStatePaused) {
 	stateman test_state_stack;
 	EXPECT_EQ(test_state_stack.no_of_states(), 0);
-	int test_me,test_me_2;
+	int test_me, test_me_2;
 	test_state_stack.Add(std::make_unique<dummy_state>(test_me));
 	test_state_stack.ProcessStatechange();
 	test_state_stack.Add(std::make_unique<dummy_state>(test_me_2));
@@ -86,7 +90,7 @@ TEST(Assetholder_DeathTest, invalidgetfont) {
 
 TEST(Assetholder_DeathTest, invalidgettexture) {
 	asset_holder test_assets;
-	EXPECT_DEATH(test_assets.get_texture(asset_holder::group_member_name::OJJAS,0), "Assertion failed.*");
+	EXPECT_DEATH(test_assets.get_texture(asset_holder::group_member_name::OJJAS, 0), "Assertion failed.*");
 }
 
 TEST(Assetholder_DeathTest, invalidgetsoundbuffer) {
@@ -97,4 +101,28 @@ TEST(Assetholder_DeathTest, invalidgetsoundbuffer) {
 TEST(Assetholder_DeathTest, invalidplaysound) {
 	asset_holder test_assets;
 	EXPECT_DEATH(test_assets.play_sound(asset_holder::group_member_name::OJJAS, 0), "Assertion failed.*");
+}
+
+TEST(AssetholderProperLoading, validfont) {
+	asset_holder test_assets;
+	test_assets.add_font(0, "Fonts/Dosis-ExtraLight.otf");
+	EXPECT_NO_CRASH(test_assets.get_font(0));
+}
+
+TEST(AssetholderProperLoading, validtexture) {
+	asset_holder test_assets;
+	test_assets.add_texture(asset_holder::group_member_name::OJJAS, asset_holder::ojjas_textures::SPRING, "Images/spring.png");
+	EXPECT_NO_CRASH(test_assets.get_texture(asset_holder::group_member_name::OJJAS, asset_holder::ojjas_textures::SPRING));
+}
+
+TEST(AssetholderProperLoading, validsoundbuffer) {
+	asset_holder test_assets;
+	test_assets.add_sound_buffer(asset_holder::group_member_name::OJJAS, asset_holder::ojjas_sounds::BUTTON_FORWARD, "Sounds/button_forward.flac");
+	EXPECT_NO_CRASH(test_assets.get_soundbuffer(asset_holder::group_member_name::OJJAS, asset_holder::ojjas_sounds::BUTTON_FORWARD));
+}
+
+TEST(AssetholderProperLoading, validplay) {
+	asset_holder test_assets;
+	test_assets.add_sound_buffer(asset_holder::group_member_name::OJJAS, asset_holder::ojjas_sounds::BUTTON_FORWARD, "Sounds/button_forward.flac");
+	EXPECT_NO_CRASH(test_assets.play_sound(asset_holder::group_member_name::OJJAS, asset_holder::ojjas_sounds::BUTTON_FORWARD));
 }
